e77a421004e4a889a35da59db4403ed2e2be3928
kashiash <kashiash@gmail.com>
2022-06-15 19:38:04 +0200

    Initial Commit

---
+++ SimpleNews/SimpleNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNewsApp.swift
+//  SimpleNews
+//
+//  Created by Jacek on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
